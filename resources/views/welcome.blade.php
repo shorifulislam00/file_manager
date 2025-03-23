c2d07c4e3fbe94153b630abcee1b6dd69234ec83
Shoriful Islam <shorifulislam433@gmail.com>
2025-03-23 11:23:18 +0600

    file manager project initialize with laravel voyager package

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>ALGFL-BD</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+</head>
+<body>
+  <div class="container d-flex justify-content-center align-items-center vh-100">
+    <div class="text-center">
+      <h1 class="display-3 text-primary">ALGFL!</h1>
+      <p class="lead">Welcome to the world of ALGFL</p>
+      {{-- <button class="btn btn-success">Click Me</button> --}}
+    </div>
+  </div>
+
+  <!-- Bootstrap JS (Optional for components like modals or dropdowns) -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
